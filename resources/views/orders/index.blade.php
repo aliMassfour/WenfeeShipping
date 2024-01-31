a525a6862698eed9c8b87423e8ed7b281cb3f6c3
ALI <assfourali98@gmail.com>
2024-01-30 21:36:13 -0800

    add some changes to index order view

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -11,9 +11,10 @@
                         <div class="card" data-bs-toggle="modal" data-bs-target="#orderModal{{$order->id}}">
                             <div class="card-header border bg-gradient-dark"></div>
                             <div class="card-body">
-                                <h4 class="card-text">number: {{$order->id}}</h4>
+                                <h4 class="card-text">number: {{$order->number}}</h4>
                                 <h6 class="card-text">name: {{$order->buyer_name}}</h6>
                                 <h6 class="card-text">phone: {{$order->buyer_phone}}</h6>
+                                <h6 class="card-text">email: {{$order->buyer_email}}</h6>
                             </div>
                             <div class="card-footer border bg-gradient-dark"></div>
                         </div>
@@ -29,28 +30,30 @@
                                     <button type="button" class="btn-close btn-danger border-0" data-bs-dismiss="modal">
                                     </button>
                                 </div>
-                                <div class="modal-body">
+                                <div class="modal-body  ">
                                     <!-- Add order details here -->
                                     <p>Name: {{$order->buyer_name}}</p>
                                     <p>Phone: {{$order->buyer_phone}}</p>
-                                    <p>products :</p>
+                                    <p>Products:</p>
                                     <ul class="list-group">
                                         @foreach(json_decode($order->products) as $product)
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                {{$product->name}}
-                                                <span
-                                                    class="badge bg-gradient-dark rounded-pill">{{$product->amount}}</span>
-                                                <span class="badge bg-gradient-dark rounded-pill">{{$product->amount * $product->price}}$</span>
+                                                <span>{{$product->name}}</span>
+                                                <div
+                                                    class="d-sm-inline-flex justify-content-center align-content-center ">
+                                                    <span
+                                                        class="badge bg-gradient-dark rounded-pill">{{$product->amount}}</span>
+                                                    <span class="badge bg-gradient-dark rounded-pill">{{$product->amount * $product->price}}$</span>
+                                                </div>
                                             </li>
                                         @endforeach
-
-
                                     </ul>
 
                                 </div>
                                 <div class="modal-footer">
                                     @if($order->status == "not_delivered")
-                                        <a href="{{route('delivery.create',$order->id)}}" class="btn bg-gradient-dark"> make
+                                        <a href="{{route('delivery.create',$order->id)}}" class="btn bg-gradient-dark">
+                                            make
                                             delivery</a>
                                     @endif
                                     <button type="button" class="btn bg-gradient-dark" data-bs-dismiss="modal">Close
